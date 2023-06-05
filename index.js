367811b8d684a8bff991eb62431b2f7cbf4a65ec
Harrison_Hilson <harryhilson8@gmail.com>
2023-06-05 17:43:25 -0400

    js DOM

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const m = document.getElementById("main")
+m.remove();
+const newHeader = document.createElement("h1");
+newHeader.id= "victory"
+newHeader.textContent="Harry is the champion"
+document.body.append(newHeader);
